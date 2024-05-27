ffcafe1f852384961b1db0ce815928d802614a61
saragrosser <sgrosser00@gmail.com>
2024-05-27 18:10:50 +0200

    Offline Capability

--- src/api.js
+++ src/api.js
@@ -30,9 +30,12 @@ export const getEvents = async () => {
   if (window.location.href.startsWith("http://localhost")) {
     return mockData;
   }
-
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events ? JSON.parse(events) : [];
+  }
   const token = await getAccessToken();
-
   if (token) {
     removeQuery();
     const url =
@@ -42,6 +45,8 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
